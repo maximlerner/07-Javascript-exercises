ff52a52029490fbc1a95b0a96a410c2b4ce3a557
Maximlerner <maximus555778@gmail.com>
2023-05-15 08:40:39 +0300

    Update section 17

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,4 @@
-var budget = [
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,58 +7,47 @@ var budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-var limits = {
+// Object.freeze makes the object immutable(can't add new property)
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+// This is not going to work
+// spendingLimits.jay = 500;
+console.log(spendingLimits);
+
+const getLimit = (limits,user) => limits?.[user] ? spendingLimits[user] : 0;
+
+// Pure function
+const addExpense = function (state,limits,value, description, user = 'jonas') {
+
+  const cleanUser = user.toLowerCase();
+
+  //Here we return new copy
+  return value <= getLimit(limits,cleanUser) ? [...state,{ value: -value, description, user: cleanUser}] : state;
+
 };
+const newBudget1 = addExpense(budget,spendingLimits,10, 'Pizza 🍕');
+const newBudget2 = addExpense(newBudget1,spendingLimits,100, 'Going to movies 🍿', 'Matilda');
+const newBudget3 = addExpense(newBudget2,spendingLimits,200, 'Stuff', 'Jay');
+console.log(newBudget1);
+console.log(newBudget2);
+console.log(newBudget3);
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
-  user = user.toLowerCase();
-
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
-
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
+const checkExpenses = (state,limits) => {
+  return state.map(entry => {
+    return entry.value < -getLimit(limits,entry.user) ? {...entry,flag: 'limit'} :entry;
+  })
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
+const finalBudget = checkExpenses(newBudget3,spendingLimits);
+console.log(finalBudget);
+
+const logBigExpenses = function (state,bigLimit) {
+  const bigExpenses = state.filter(entry => entry.value <= -bigLimit).map(entry => entry.description.slice(-2)).join(' / ');
+
+  console.log(bigExpenses);
+};
 console.log(budget);
-
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
-
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
-};
-check();
-
-console.log(budget);
-
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
+logBigExpenses(finalBudget,500)
